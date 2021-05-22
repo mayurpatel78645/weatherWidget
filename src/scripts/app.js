4fa3b48f24f9183229e84e6404ff6aea57d12b68
mayurpatel78645 <mayurpatel78645@gmail.com>
2021-05-22 14:54:39 -0500

    render current conditions

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,108 @@
 const apiKey = 'f4b85b43229c9533232a8803096419fa';
 
-const getLocation = async() => {
+class Forecast {
+  static getLocation() {
+    if('geolocation' in navigator) {
+      navigator.geolocation.watchPosition(this.success, this.error);
+    } else {
+      console.log('geolocation IS NOT available');
+    }
+  }
+
+  static error = () => {
+    console.error(`Unable to retrieve location`);
+  }
+
+  static success = (position) => {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`
+    this.getForecastData(url);
+  }
+
+  static getForecastData = async(url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  }
+}
+
+class CurrentWeather {
+  static getLocation() {
+    if('geolocation' in navigator) {
+      navigator.geolocation.watchPosition(this.success, this.error);
+    } else {
+      console.log('geolocation IS NOT available');
+    }
+  }
+
+  static error = () => {
+    console.error(`Unable to retrieve location`);
+  }
+
+  static success = (position) => {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+    this.currentWeatherData(url);
+  }
+
+  static currentWeatherData = async(url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    this.render(data);
+  }
+
+  static render = (data) => {
+    const currentConditions = document.querySelector('.current-conditions');
+    const temp = data.main.temp;
+    const condition = data.weather[0].description;
+    const icon = data.weather[0].icon;
+    console.log(data);
+    currentConditions.innerHTML = '';
+    currentConditions.insertAdjacentHTML('beforeend',
+    `
+    <h2>Current Conditions</h2>
+        <img src="http://openweathermap.org/img/wn/${icon}@2x.png" />
+        <div class="current">
+          <div class="temp">${temp}℃</div>
+          <div class="condition">${condition}</div>
+        </div>
+    `);
+  }
+}
+
+Forecast.getLocation();
+CurrentWeather.getLocation();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const getLocation = async() => {
   if('geolocation' in navigator) {
     navigator.geolocation.watchPosition((position) => {
       userLocation(position);
@@ -11,7 +113,7 @@ const getLocation = async() => {
 }
 
 const getCurrentWeatherData = async(latitude, longitude) => {
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
   const response = await fetch(url);
   const data = await response.json();
   return data;
@@ -23,6 +125,4 @@ const userLocation = async(position) => {
   console.log(latitude, longitude);
   const weatherData = await getCurrentWeatherData(latitude, longitude);
   console.log(weatherData);
-}
-
-getLocation();
+}*/
